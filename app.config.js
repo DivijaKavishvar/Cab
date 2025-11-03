e2ebc6b602b4a54050f2f7ad7b1679dd20a6587f
DivijaKavishvar <divijakavishvar@gmail.com>
2025-11-03 22:35:13 +0530

    Fix app.config.js content with env-based configuration

--- app.config.js
+++ app.config.js
@@ -1,56 +1,50 @@
-{
-  "expo": {
-    "name": "bolt-expo-nativewind",
-    "slug": "bolt-expo-nativewind",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/images/icon.png",
-    "scheme": "myapp",
-    "userInterfaceStyle": "automatic",
-    "newArchEnabled": true,
-    "ios": {
-      "supportsTablet": true,
-      "config": {
-        "googleMapsApiKey": "REMOVED_USE_ENV"
-      },
-      "infoPlist": {
-        "NSLocationWhenInUseUsageDescription": "We use your location to show nearby rides."
-      },
-      "bundleIdentifier": "com.divijakavishvar.cabby" 
-    },
-    "android": {
-      "config": {
-        "googleMaps": {
-          "apiKey": "REMOVED_USE_ENV"
-        }
-      },
-      "permissions": [
-        "ACCESS_COARSE_LOCATION",
-        "ACCESS_FINE_LOCATION"
-      ],
-      "package": "com.divijakavishvar.cabby",   
-      "versionCode": 1                          
-    },
-    "web": {
-      "bundler": "metro",
-      "output": "single",
-      "favicon": "./assets/images/favicon.png"
-    },
-    "plugins": [
-      "expo-router",
-      "expo-font",
-      "expo-web-browser",
-      "expo-location"
-    ],
-    "experiments": {
-      "typedRoutes": true
-    },
-    "extra": {
-      "router": {},
-      "eas": {
-        "projectId": "ada921ec-b3dc-402c-ba70-ab4fa949eb75"
-      }
-    }
-  }
-}
+// app.config.js
+import 'dotenv/config';
 
+export default {
+  expo: {
+    name: "bolt-expo-nativewind",
+    slug: "bolt-expo-nativewind",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+
+    ios: {
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+      },
+      infoPlist: {
+        NSLocationWhenInUseUsageDescription:
+          "We use your location to show nearby rides.",
+      },
+      bundleIdentifier: "com.divijakavishvar.cabby",
+    },
+
+    android: {
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY,
+        },
+      },
+      permissions: ["ACCESS_COARSE_LOCATION", "ACCESS_FINE_LOCATION"],
+      package: "com.divijakavishvar.cabby",
+      versionCode: 1,
+    },
+
+    web: {
+      bundler: "metro",
+      output: "single",
+      favicon: "./assets/images/favicon.png",
+    },
+
+    plugins: ["expo-router", "expo-font", "expo-web-browser", "expo-location"],
+
+    experiments: {
+      typedRoutes: true,
+    },
+  },
+};
